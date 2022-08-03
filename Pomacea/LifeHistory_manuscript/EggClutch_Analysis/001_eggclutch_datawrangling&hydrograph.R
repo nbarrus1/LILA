ef7de43fb7fc9ffc8cb16e1e2ec3c42e826fc05c
Nathan T. Barrus <nbarrus1@gmail.com>
2022-08-03 08:35:59 -0400

    Create 001_eggclutch_datawrangling&hydrograph.R

---
+++ Pomacea/LifeHistory_manuscript/EggClutch_Analysis/001_eggclutch_datawrangling&hydrograph.R
@@ -0,0 +1,13 @@
+rm(list = ls())  #clears global environment
+
+####Important!!! packages that I have below will need to be installed if not installed 
+####previously
+
+#------------------------------------------------
+#####libraries#####
+#------------------------------------------------
+
+library(tidyverse)       #package for languange and syntax that makes code a little more friendly
+                         #includes a host of other packages like dplyr, ggplot2, etc.
+library(readxl)          #package used to read excel formatted data
+library(lubridate)       #library used to make working with date easier
